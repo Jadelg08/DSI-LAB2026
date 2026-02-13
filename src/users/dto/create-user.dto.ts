dc926e4cf2980ac237111e1e43fd11aa1ef32fc1
Jade González <Jade.LGonzalez@outlook.com>
2026-02-12 20:03:31 -0600

    Guia 2- CRUD Completo con Swagger y validaciones

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, IsInt } from 'class-validator';
+
+export class CreateUserDto {
+
+  @ApiProperty({ example: 'Jade Gonzalez' })
+   @IsString()
+  name: string;
+
+  @ApiProperty({ example: 'jade@email.com' })
+    @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 1 })
+    @IsInt()
+  tenantId: number;
+}
